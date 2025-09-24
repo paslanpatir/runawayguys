6f8967c52f49977add593c8641b9b00ac6830c11
PelinYurdadon <plnyrddn@hotmail.com>
2025-09-25 02:05:30 +0300

    bug_fix: get_test is converted to get

--- src/ask_boyfriend_name.py
+++ src/ask_boyfriend_name.py
@@ -9,13 +9,13 @@ class AskBoyfriendName(BaseStep):
         language = st.session_state.user_details.get("language", "EN")
         msg = Message(language)
 
-        st.write(msg.get_text("enter_bf_name_msg"))
+        st.write(msg.get("enter_bf_name_msg"))
 
         with st.form("bf_name_form"):
-            bf_name = st.text_input(msg.get_text("bf_name_input"), key="bf_name_input")
-            if st.form_submit_button(msg.get_text("continue_msg")):
+            bf_name = st.text_input(msg.get("bf_name_input"), key="bf_name_input")
+            if st.form_submit_button(msg.get("continue_msg")):
                 if bf_name:
                     st.session_state.user_details["bf_name"] = bf_name
                     st.rerun()
                 else:
-                    st.error(msg.get_text("enter_bf_name_msg"))
+                    st.error(msg.get("enter_bf_name_msg"))
